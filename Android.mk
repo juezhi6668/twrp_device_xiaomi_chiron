44a313e710d7f5b3e56c346f8c5d17eedcb0c1ea
juezhi6668 <1649417471@qq.com>
2026-01-20 22:52:08 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -30,13 +30,10 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 LOCAL_CHECK_ELF_FILES := false
-LOCAL_MODULE := android.hardware.gatekeeper@1.0-service_recovery
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
-LOCAL_MODULE_STEM := android.hardware.gatekeeper@1.0-service
-LOCAL_REQUIRED_MODULES := android.hardware.gatekeeper@1.0-service android.hardware.keymaster@3.0-service_recovery
-LOCAL_SRC_FILES := ../../../$(TARGET_OUT_VENDOR_EXECUTABLES)/hw/android.hardware.gatekeeper@1.0-service
+LOCAL_REQUIRED_MODULES := android.hardware.keymaster@3.0-service_recovery
 LOCAL_POST_INSTALL_CMD := \
     touch $(TARGET_RECOVERY_ROOT_OUT)/system/bin/qseecomd; \
     bash device/qcom/twrp-common/scripts/service_cleanup.bash;
